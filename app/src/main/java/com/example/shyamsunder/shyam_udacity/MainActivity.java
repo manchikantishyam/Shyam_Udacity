d73afd8bff3df0f963a5d2e42e0cda7cb0aba99f
Shyam sunder Manchikanti <manchikantishyam@gmail.com>
2015-08-08 17:42:05 -0400

    Updated Changes as mentioned in the comments

--- app/src/main/java/com/example/shyamsunder/shyam_udacity/MainActivity.java
+++ app/src/main/java/com/example/shyamsunder/shyam_udacity/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.shyamsunder.shyam_udacity;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 
@@ -15,38 +13,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button spotify_Streamer = (Button)findViewById(R.id.spotify_streamer);
-        Button scores_App = (Button)findViewById(R.id.scores_app);
-        Button library_App = (Button)findViewById(R.id.library_app);
-        Button build_it_Bigger = (Button)findViewById(R.id.build_itbig);
-        Button capstone_App = (Button)findViewById(R.id.capstone_myApp);
-
-        spotify_Streamer.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                showToast("Spotify Streamer is Under Construction");
-            }
-        });
-        scores_App.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                showToast("Scores App is Under Construction");
-            }
-        });
-        library_App.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                showToast("Library App is Under Construction");
-            }
-        });
-        build_it_Bigger.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                showToast("Build It Bigger is Under Construction");
-            }
-        });
-        capstone_App.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                showToast("My Capstone App is  Under Construction");
-            }
-        });
-
     }
 
     @Override
@@ -56,22 +22,25 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
     public void showToast(String toastString){
         Toast.makeText(this.getBaseContext(), toastString,
                 Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void popularMoviesClick(View v){
+        showToast("PopularMovies is Under Construction");
+    }
+    public void scoresAppClick(View v){
+        showToast("Scores App is Under Construction");
+    }
+    public void buildItBigClick(View v){
+        showToast("Build It Bigger is Under Construction");
+    }
+    public void libraryAppClick(View v){
+        showToast("Library App is Under Construction");
+    }
+    public void capstoneAppClick(View v){
+        showToast("My Capstone App is  Under Construction");
     }
 }
